f0c1b6c03ee916c7728c0752405dc6b51d6c1fa4
Stan1758 <109434834+Stan1758@users.noreply.github.com>
2025-05-10 20:20:40 +0000

    Deploying to gh-pages from @ Stan1758/goit-js-hw-12@a07ec50acc113e0e542768d2a9e137b12f6b3288 🚀

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-/* empty css                      */(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import{a as c,S as u,i as a}from"./assets/vendor-CrlV4O_2.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))e(t);new MutationObserver(t=>{for(const o of t)if(o.type==="childList")for(const i of o.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&e(i)}).observe(document,{childList:!0,subtree:!0});function n(t){const o={};return t.integrity&&(o.integrity=t.integrity),t.referrerPolicy&&(o.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?o.credentials="include":t.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function e(t){if(t.ep)return;t.ep=!0;const o=n(t);fetch(t.href,o)}})();const p="50197209-2a2796b344803c480c3c957e7",d="https://pixabay.com/api/";async function f(r){const s={key:p,q:r,image_type:"photo",orientation:"horizontal",safesearch:!0};try{return(await c.get(d,{params:s})).data}catch(n){throw console.error("Помилка при запиті до Pixabay:",n),n}}document.querySelector(".gallery");document.querySelector(".loader");const y=new u(".gallery a");function m(){return document.querySelector(".gallery")}function g(r){const s=document.querySelector(".gallery"),n=r.map(e=>`
+    <li class="gallery-item">
+      <a href="${e.largeImageURL}" title="${e.tags}">
+        <img src="${e.webformatURL}" alt="${e.tags}" loading="lazy" />
+      </a>
+      <div class="info">
+  <div class="info-item">
+    <p><b>Likes</b></p>
+    <p class="value">${e.likes}</p>
+  </div>
+  <div class="info-item">
+    <p><b>Views</b></p>
+    <p class="value">${e.views}</p>
+  </div>
+  <div class="info-item">
+    <p><b>Comments</b></p>
+    <p class="value">${e.comments}</p>
+  </div>
+  <div class="info-item">
+    <p><b>Downloads</b></p>
+    <p class="value">${e.downloads}</p>
+  </div>
+</div>
+    </li>
+  `).join("");s.insertAdjacentHTML("beforeend",n),y.refresh()}function h(){const r=m();r.innerHTML=""}function b(){const r=document.querySelector(".loader-wrapper");r&&(r.style.display="flex")}function v(){const r=document.querySelector(".loader-wrapper");r&&(r.style.display="none")}const l=document.querySelector(".form");l.addEventListener("submit",r=>{r.preventDefault();const n=l.elements["search-text"].value.trim();if(!n){a.warning({title:"Hello!",message:"The search field cannot be empty",position:"topRight"});return}h(),b(),f(n).then(e=>{if(!e.hits||!e.hits.length){a.info({title:"",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"});return}g(e.hits)}).catch(e=>{a.error({title:"Error",message:"Something went wrong, please try again.",position:"topRight"})}).finally(()=>{v()})});
 //# sourceMappingURL=index.js.map
